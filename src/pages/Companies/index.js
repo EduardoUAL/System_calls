216b189a200bf4e0bfbe71f1046a7434d41e70cc
EduardoUAL <30007524@students.ual.pt>
2024-05-31 10:54:46 +0100

    Update index.js

--- src/pages/Companies/index.js
+++ src/pages/Companies/index.js
@@ -1,12 +1,47 @@
-import Header from "../../components/Header";
-import Title from "../../components/Title";
+import { useState } from 'react'
 
 //-- React Icons
 import { FiUser } from 'react-icons/fi'
 
-export default function Companies(){
-    
+//-- Components
+import Header from '../../components/Header'
+import Title from '../../components/Title'
 
+//-- firebase
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '../../services/firebaseConnection'
+
+//--Toast
+import { toast } from 'react-toastify'
+
+export default function Companies(){
+    const [ nome, setNome ] = useState('') 
+    const [ nipc, setnipc] = useState('') 
+    const [ morada, setmorada] = useState('') 
+
+    async function handleRegister(e){
+        e.preventDefault()
+
+        if(nome !== '' && cc !== '' && morada !== ''){
+            await addDoc(collection(db, "Companies"), {
+                NickName: nome,
+                nipc: nipc,
+                morada: morada
+            })
+            .then(() => {
+                setNome('')
+                setnipc('')
+                setmorada('')
+                toast.success('Empresa registada com sucesso!')
+            })
+            .catch((error) => {
+                console.log(error)
+                toast.error('Erro ao fazer o registo!')
+            })
+        } else {
+            toast.error('Preencha todos os campos!')
+        }
+    }
     return(
         <div>
             <Header />
@@ -15,9 +50,37 @@ export default function Companies(){
                 <Title name = "Empresas">
                    <FiUser size={25} />
                 </Title>
-            </div>
-            
 
+            <div className="container">
+                <form className="form-profile" onSubmit={handleRegister} >
+                    <label>Nome</label>
+                    <input 
+                        type='text'
+                        placeholder="Nome da empresa"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                    />
+
+                    <label>nipc</label>
+                    <input 
+                        type='text'
+                        placeholder='Introduza o seu nipc '
+                        value={nipc}
+                        onChange={(e) => setcc(e.target.value)}
+                    />
+
+                    <label>morada</label>
+                    <input 
+                        type='text'
+                        placeholder='Introduza a sua morada'
+                        value={morada}
+                        onChange={(e) => setmorada(e.target.value)}
+                    />
+
+                    <button type='submit'>Salvar</button>
+                </form>
+            </div>
+            </div>
         </div>
     )
 }
