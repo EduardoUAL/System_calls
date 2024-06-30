75dc670e368a717230b5b9b61007cd2e64c83de3
joana <92855195+joannagraca@users.noreply.github.com>
2024-06-30 23:21:07 +0100

    Update index.js

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -6,87 +6,85 @@ import { AuthContext } from '../../contexts/auth'
 import logo from '../../assets/logo.png'
 
 export default function SignUp() {
-    const [name, setName] = useState('')
-    const [email,setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [errorMessage, setErrorMessage] = useState('')
+  const [name, setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const {signUp, loadingAuth} = useContext(AuthContext)
 
-    const {signUp, loadingAuth} = useContext(AuthContext)
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  async function handleSubmit(e) {
+      e.preventDefault()
+      
+      if(name !== '' && password !== '' &&  email !== '') {
+          if(pattern.test(email)) {
+              if(password === confirmPassword) {
+                  try {
+                      await signUp(email, password, name)
+                  } catch (errorMessage) {
+                      setErrorMessage('Erro ao criar conta.')
+                  }
+              } else {
+                      setErrorMessage('As senhas não são iguais.');
+              }
+          } else {
+              setErrorMessage('Email inválido.');
+              }
+      } else {
+          setErrorMessage('Preencha todos os campos');
+          }
+      }
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-        
-        if(name !== '' && password !== '' &&  email !== '' && confirmPassword !== '') {
-            if(pattern.test(email)) {
-                if(password === confirmPassword) {
-                    try {
-                        await signUp(email, password, name)
-                    } catch (errorMessage) {
-                        setErrorMessage('Erro ao criar conta.')
-                    }
-                } else {
-                        setErrorMessage('As senhas não são iguais.');
-                }
-            } else {
-                setErrorMessage('Email inválido.');
-                }
-        } else {
-            setErrorMessage('Preencha todos os campos');
-            }
-        }
-
-        
-    }
-    return (
-        <div className='container-center'>
-          <div className='login'>
-            <div className='login-area'>    
-              <img src={logo} alt="logo do sistemas de Pedidos"/>
-            </div>
-
-                
-            <form onSubmit={handleSubmit}>
-              <h1>Criar conta</h1>
       
-              <input 
-                type='text'
-                placeholder='Introduzir nome'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-      
-              <input 
-                type='text'
-                placeholder='Introduzir email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-      
-              <input 
-                type='password'
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-      
-              <input 
-                type='password'
-                placeholder='Confirmar password'
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-      
-              <button type='submit'> 
-                {loadingAuth ? 'A carregar' : 'Registar'} 
-              </button>
-            
-            </form>
-      
-            <Link to="/"> Ja tem uma conta? Faça login! </Link>
+  }
+  return (
+      <div className='container-center'>
+        <div className='login'>
+          <div className='login-area'>    
+            <img src={logo} alt="logo do sistemas de Pedidos"/>
           </div>
+
+              
+          <form onSubmit={handleSubmit}>
+            <h1>Criar conta</h1>
+    
+            <input 
+              type='text'
+              placeholder='Introduzir nome'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+    
+            <input 
+              type='text'
+              placeholder='Introduzir email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+    
+            <input 
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+    
+            <input 
+              type='password'
+              placeholder='Confirmar password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+    
+            <button type='submit'> 
+              {loadingAuth ? 'A carregar' : 'Registar'} 
+            </button>
+          
+          </form>
+    
+          <Link to="/"> Ja tem uma conta? Faça login! </Link>
         </div>
-      )
+      </div>
+    )
 
