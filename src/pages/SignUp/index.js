78317d6d685bc6f1453fdaa6468a709a8c3f5401
joana <92855195+joannagraca@users.noreply.github.com>
2024-06-30 16:28:17 +0100

    Update index.js

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -45,14 +45,14 @@ export default function SignUp(){
                     />
 
                     <input 
-                        type='text'
+                        type='password'
                         placeholder='Password'
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
 
                     <input 
-                        type='text'
+                        type='password'
                         placeholder='Confirmar password'
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
